59a8dda52ed8f91ee1cc71f5323adacd6f73ed49
zhangxy6 <zhangxy6@shanghaitech.edu.cn>
2022-05-25 23:49:45 +0800

    说明

---
+++ data_utils.py
@@ -0,0 +1,192 @@
+﻿import os.path
+import json
+import scipy
+from scipy.special import jn_zeros,jn,sph_harm
+import numpy as np
+import pandas as pd
+import glob
+import torch
+from torch.utils.data import Dataset
+import tqdm
+
+def load_graph_data(file_path):
+    Total={}
+    for path in file_path:
+        print('loading : {}'.format(path))
+        try:
+            graphs = np.load(path,allow_pickle=True)['graph_dict'].item()
+        except UnicodeError:
+            graphs = np.load(path, encoding='latin1',allow_pickle=True)['graph_dict'].item()
+            graphs = { k.decode() : v for k, v in graphs.items() }
+        Total={**Total,**graphs}
+    #print (Total)
+    print('load successed, final volume : {}'.format(len(Total)))
+    return Total
+
+
+
+def Atomgraph_collate(batch):
+    nodes = []
+    edge_distance=[]
+    edge_targets=[]
+    edge_sources = []
+    graph_indices = []
+    node_counts = []
+    targets = []
+    combine_sets =[]
+    plane_wave = []
+    total_count = 0
+
+    for i, (graph, target) in enumerate(batch):
+
+        # Numbering for each batch
+        nodes.append(graph.nodes) 
+        edge_distance.append(graph.distance) 
+        edge_sources.append(graph.edge_sources + total_count) # source number of each edge
+        edge_targets.append(graph.edge_targets + total_count) # target number of each edge
+        combine_sets.append(graph.combine_sets)
+        plane_wave.append(graph.plane_wave)
+        node_counts.append(len(graph))
+        targets.append(target)
+        graph_indices += [i] * len(graph)
+        total_count += len(graph)
+
+    combine_sets=np.concatenate(combine_sets,axis=0)
+    plane_wave=np.concatenate(plane_wave,axis=0)
+    nodes = np.concatenate(nodes,axis=0)
+    edge_distance = np.concatenate(edge_distance,axis=0)
+    edge_sources = np.concatenate(edge_sources,axis=0)
+    edge_targets = np.concatenate(edge_targets,axis=0)
+    input = geo_CGNN_Input(nodes,edge_distance, edge_sources, edge_targets, graph_indices, node_counts,combine_sets,plane_wave)
+    targets = torch.Tensor(targets)
+    return input, targets
+
+class AtomGraph(object):
+    def __init__(self, graph,cutoff,N_shbf,N_srbf,n_grid_K,n_Gaussian):
+        lattice, self.nodes, neighbors,volume = graph
+        nei=neighbors[0] 
+        distance=neighbors[1] 
+        vector=neighbors[2] 
+        n_nodes = len(self.nodes) 
+        self.nodes = np.array(self.nodes, dtype=np.float32)
+        self.edge_sources = np.concatenate([[i] * len(nei[i]) for i in range(n_nodes)])
+        self.edge_targets=np.concatenate(nei)
+        edge_vector = np.array(vector, dtype=np.float32)
+        self.edge_index = np.concatenate([range(len(nei[i])) for i in range(n_nodes)])
+        self.vectorij= edge_vector[self.edge_sources,self.edge_index]
+        edge_distance = np.array(distance, dtype=np.float32)
+        self.distance= edge_distance[self.edge_sources,self.edge_index]
+        combine_sets=[]
+        # gaussian radial
+        N=n_Gaussian
+        for n in range(1,N+1):
+            phi=Phi(self.distance,cutoff)
+            G=gaussian(self.distance,miuk(n,N,cutoff),betak(N,cutoff))
+            combine_sets.append(phi*G)
+        self.combine_sets=np.array(combine_sets, dtype=np.float32).transpose()
+
+        # plane wave
+        grid=n_grid_K
+        kr=np.dot(self.vectorij,get_Kpoints_random(grid,lattice,volume).transpose()) 
+        self.plane_wave=np.cos(kr)/np.sqrt(volume)
+
+    def __len__(self):
+        return len(self.nodes)
+class AtomGraphDataset(Dataset):
+    def __init__(self, path, filename,database, target_name,cutoff,N_shbf,N_srbf,n_grid_K,n_Gaussian):
+        super(AtomGraphDataset, self).__init__()
+        
+
+        target_path = os.path.join(path, "targets_"+database+".csv")
+
+        if target_name == 'band_gap' and database=='MP':
+            target_path = os.path.join(path, "targets_"+database+'_Eg'+".csv")
+        elif target_name == 'formation_energy_per_atom' and database=='MP':
+            target_path = os.path.join(path, "targets_"+database+'_Ef'+".csv")
+
+        df = pd.read_csv(target_path).dropna(axis=0,how='any') 
+
+        if target_name == 'band_gap' and (database=='OQMD' or database=='MEGNet_2018'):
+            df=df[df['band_gap']!=0]  ##############target
+
+                     
+        graph_data_path = sorted(glob.glob(os.path.join(path, 'npz/'+filename+'*.npz')))
+        print('The number of files = {}'.format(len(graph_data_path)))
+        self.graph_data = load_graph_data(graph_data_path)
+        print (len(self.graph_data))
+        graphs=self.graph_data.keys()
+        graphs=[int(i) for i in graphs]
+        print (len(graphs))
+        self.graph_names=df.loc[df['id'].isin(graphs)].id.values.tolist()
+        self.targets=np.array(df.loc[df['id'].isin(graphs)][target_name].values.tolist())  ############# finall targets
+        self.targets=np.loadtxt('all_data.txt')[:,12:]
+        print('the number of valid targets = {}'.format(len(self.targets)))
+        print('start to constructe AtomGraph')
+        graph_data=[]
+      #  print (self.graph_names)
+        for i,name in enumerate(self.graph_names):
+            graph_data.append(AtomGraph(self.graph_data[str(name)],cutoff,N_shbf,N_srbf,n_grid_K,n_Gaussian))
+            if i%2000==0 and i>0:
+                print('{} graphs constructed'.format(i))
+        print('finish constructe the graph')
+        self.graph_data=graph_data
+      #  print (self.targets.shape[0],(len(self.graph_data)))           
+        assert(len(self.graph_data)==self.targets.shape[0]    )#len(self.targets))
+        print('The number of valid graphs = {}'.format(self.targets.shape[0]))
+                
+    def __getitem__(self, index):
+        return self.graph_data[index], self.targets[index,:]
+
+    def __len__(self):
+        return len(self.graph_names)
+
+
+# 构建torch的输入张量
+class geo_CGNN_Input(object):
+    def __init__(self,nodes,edge_distance,edge_sources, edge_targets, graph_indices, node_counts,combine_sets,plane_wave):
+        self.nodes = torch.Tensor(nodes)
+        self.edge_distance = torch.Tensor(edge_distance)
+        self.edge_sources = torch.LongTensor(edge_sources)
+        self.edge_targets = torch.LongTensor(edge_targets)
+        self.graph_indices = torch.LongTensor(graph_indices)
+        self.node_counts = torch.Tensor(node_counts)
+        self.combine_sets=torch.Tensor(combine_sets)
+        self.plane_wave=torch.Tensor(plane_wave)
+
+    def __len__(self):
+        return self.nodes.size(0)
+
+
+def a_SBF(alpha,l,n,d,cutoff):
+    root=float(jn_zeros(l,n)[n-1])
+    return jn(l,root*d/cutoff)*sph_harm(0,l,np.array(alpha),0).real*np.sqrt(2/cutoff**3/jn(l+1,root)**2)
+
+def a_RBF(n,d,cutoff):
+    return np.sqrt(2/cutoff)*np.sin(n*np.pi*d/cutoff)/d
+
+def get_Kpoints_random(q,lattice,volume):
+    a0=lattice[0,:]
+    a1=lattice[1,:]
+    a2=lattice[2,:]
+    unit=2*np.pi*np.vstack((np.cross(a1,a2),np.cross(a2,a0),np.cross(a0,a1)))/volume
+    ur=[(2*r-q-1)/2/q for r in range(1,q+1)]
+    points=[]
+    for i in ur:
+        for j in ur:
+            for k in ur:
+                points.append(unit[0,:]*i+unit[1,:]*j+unit[2,:]*k)
+    points=np.array(points) 
+    return points  
+
+
+def Phi(r,cutoff):
+    return 1-6*(r/cutoff)**5+15*(r/cutoff)**4-10*(r/cutoff)**3
+def gaussian(r,miuk,betak):
+    return np.exp(-betak*(np.exp(-r)-miuk)**2)
+def miuk(n,K,cutoff):
+    # n=[1,K]
+    return np.exp(-cutoff)+(1-np.exp(-cutoff))/K*n
+def betak(K,cutoff):
+    return (2/K*(1-np.exp(-cutoff)))**(-2)
+
+
---
+++ models.py
@@ -0,0 +1,426 @@
+﻿import sys
+import torch
+import time
+import json
+import os
+import copy
+import numpy as np
+import pandas as pd
+import torch.optim as optim
+from torch.utils.data import DataLoader, Subset, SubsetRandomSampler
+import torch.nn as nn
+from torch.nn import ( Linear, Bilinear, Sigmoid, Softplus, ELU, ReLU, SELU,
+                       CELU, BatchNorm1d, ModuleList, Sequential,Tanh )
+from torch.nn.modules.module import Module
+import torch.nn.functional as F
+from torch.nn.utils import clip_grad_value_
+import re
+
+def get_activation(name):
+    act_name = name.lower()
+    m = re.match(r"(\w+)\((\d+\.\d+)\)", act_name)
+    if m is not None:
+        act_name, alpha = m.groups()
+        alpha = float(alpha)
+        print(act_name, alpha)
+    else:
+        alpha = 1.0
+    if act_name == 'softplus':
+        return Softplus()
+    elif act_name == 'ssp':
+        return SSP()
+    elif act_name == 'elu':
+        return ELU(alpha)
+    elif act_name == 'relu':
+        return ReLU()
+    elif act_name == 'selu':
+        return SELU()
+    elif act_name == 'celu':
+        return CELU(alpha)
+    elif act_name == 'sigmoid':
+        return Sigmoid()
+    elif act_name == 'tanh':
+        return Tanh()
+    else:
+        raise NameError("Not supported activation: {}".format(name))
+
+
+class Metric(object):
+    def __init__(self, metric_fn, name):
+        self.metric_fn = metric_fn
+        self.name = name
+        self.total_metric = 0.0
+        self.total_count = 0
+
+    def __call__(self, predictoins, targets):
+        return self.metric_fn(predictoins, targets)
+
+    def add_batch_metric(self, predictoins, targets):
+        metric_tensor = self.metric_fn(predictoins, targets)
+        self.total_metric += metric_tensor.item() * targets.size(0)
+        self.total_count += targets.size(0)
+        return metric_tensor
+
+    def get_total_metric(self):
+        score = self.total_metric / self.total_count
+        self.total_metric = 0.0
+        self.total_count = 0
+        return score
+
+class History(object):
+    def __init__(self,file_path="data/history.csv"):
+        self.history_path = file_path
+        self.file = open(file_path, 'w')
+        self.first = True
+
+    def write(self, epoch, metrics):
+        if self.first:
+            self.first = False
+            header = ','.join([name for name, _ in metrics])
+            header = 'epoch,' + header + '\n'
+            self.file.write(header)
+        row = ','.join(['{}'.format(metric) for _, metric in metrics])
+        row = '{},'.format(epoch) + row + '\n'
+        self.file.write(row)
+        self.file.flush()
+
+    def close(self):
+        self.file.close()
+
+class Checkpoint(object):
+    def __init__(self, model):
+        self.model = model
+        self.best_metric = None
+        self.best_weights = model.weights
+
+    def check(self, metric):
+        if self.best_metric is None or metric < self.best_metric:
+            self.best_metric = metric
+            self.best_weights = self.model.weights
+
+    def restore(self):
+        self.model.weights = self.best_weights
+
+class Model(object):
+    def __init__(self, device, model, name, optimizer, scheduler, clip_value=None,
+                 metrics=[('loss', nn.MSELoss()), ('mae', nn.L1Loss())]):
+        self.name=name
+        self.model=model.to(device)
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.metrics = [Metric(metric, name) for name, metric in metrics]
+        self.clip_value = clip_value
+        self.device=device
+
+    def _set_mode(self, phase):
+        if phase == 'train':
+            self.model.train()  # Set model to training mode
+        else:
+            self.model.eval()   # Set model to evaluate mode
+
+    def _process_batch(self, input, targets, phase):
+        
+        nodes=input.nodes.to(self.device)
+        edge_sources=input.edge_sources.to(self.device)
+        edge_targets=input.edge_targets.to(self.device)
+        edge_distance=input.edge_distance.to(self.device)
+        graph_indices=input.graph_indices.to(self.device)
+        node_counts=input.node_counts.to(self.device)
+        combine_sets=input.combine_sets.to(self.device)
+        plane_wave=input.plane_wave.to(self.device)
+        targets = targets.to(self.device)        
+        self.optimizer.zero_grad()
+
+        with torch.set_grad_enabled(phase == 'train'):
+            outputs = self.model(nodes,edge_sources,edge_targets,edge_distance,graph_indices,node_counts,combine_sets,plane_wave) 
+            metric_tensors = [metric.add_batch_metric(outputs, targets) for metric in self.metrics]
+            if phase == 'train':
+                loss = metric_tensors[0]
+                loss.backward()
+                if self.clip_value is not None:
+                    clip_grad_value_(self.model.parameters(), self.clip_value)
+                self.optimizer.step()
+
+        return metric_tensors, outputs
+
+    def train(self, train_dl, val_dl, num_epochs):
+        since = time.time()
+
+        dataloaders = {'train': train_dl, 'val': val_dl}
+        history = History(file_path='data/history_{}.csv'.format(self.name))
+        checkpoint = Checkpoint(self)
+        for epoch in range(num_epochs):
+            epoch_since = time.time()
+            print('Epoch {}/{}'.format(epoch, num_epochs - 1), flush=True)
+
+            #self.scheduler.step()
+            print('current lr:', self.optimizer.param_groups[0]['lr'])
+
+            train_val_metrics = []
+            for phase in ['train', 'val']:
+                self._set_mode(phase)
+
+                for input, targets in dataloaders[phase]:
+                    _, outputs = self._process_batch(input, targets, phase)
+
+
+                epoch_metrics = [(metric.name, metric.get_total_metric()) for metric in self.metrics]
+                text = ' '.join(['{}: {:.4f}'.format(name, metric) for name, metric in epoch_metrics])
+                print('{} {}'.format(phase, text))
+
+                if phase == 'val':
+                    metric = epoch_metrics[1][1]
+                    checkpoint.check(metric)
+
+                train_val_metrics += [('_'.join([phase, name]), metric) for name, metric in epoch_metrics]
+            self.scheduler.step()
+
+            history.write(epoch, train_val_metrics)
+            time_elapsed = time.time() - epoch_since
+            print('Elapsed time (sec.): {:.3f}'.format(time_elapsed))
+            print()
+        history.close()
+
+        if num_epochs > 0:
+            time_elapsed = time.time() - since
+            print('Total elapsed time (sec.): {:.3f}'.format(time_elapsed))
+            print('The best val metric: {:4f}'.format(checkpoint.best_metric))
+            print()
+
+            # load the best model weights
+            checkpoint.restore()
+
+
+    def evaluate(self, dataloader):
+        self.model.eval()   # Set model to evaluate mode
+
+        # Iterate over data.
+        all_outputs = []
+        all_targets = []
+        all_graph_vec = []
+        for input, targets in dataloader:
+
+            nodes=input.nodes.to(self.device)
+            edge_sources=input.edge_sources.to(self.device)
+            edge_targets=input.edge_targets.to(self.device)
+            edge_distance=input.edge_distance.to(self.device)
+            graph_indices=input.graph_indices.to(self.device)
+            node_counts=input.node_counts.to(self.device)
+            combine_sets=input.combine_sets.to(self.device)
+            plane_wave=input.plane_wave.to(self.device)
+            targets = targets.to(self.device)        
+            
+            with torch.set_grad_enabled(False):
+                outputs,graph_vec = self.model(nodes,edge_sources,edge_targets,edge_distance,graph_indices,node_counts,combine_sets,plane_wave,output_graph=True)
+
+            outputs = outputs.to(torch.device("cpu")).numpy()
+            targets = targets.to(torch.device("cpu")).numpy()
+            graph_vec = graph_vec.to(torch.device("cpu")).numpy()
+            all_outputs.append(outputs)
+            all_targets.append(targets)
+            all_graph_vec.append(graph_vec)
+
+        all_outputs = np.concatenate(all_outputs)
+        all_targets = np.concatenate(all_targets)
+        all_graph_vec = np.concatenate(all_graph_vec)
+
+        all_outputs = torch.FloatTensor(all_outputs).to(self.device)
+        all_targets = torch.FloatTensor(all_targets).to(self.device)
+
+        total_metrics = [(metric.name, metric(all_outputs, all_targets).item()) for metric in self.metrics]
+        text = ' '.join(['{}: {:.4f}'.format(name, metric) for name, metric in total_metrics])
+        print('test {}'.format(text))
+
+        all_outputs = all_outputs.to(torch.device("cpu")).numpy()
+        all_targets = all_targets.to(torch.device("cpu")).numpy()
+
+        return all_outputs, all_targets, all_graph_vec
+
+    def save(self, model_path="model"):
+        model_path="model/model_{}.pth".format(self.name)
+        torch.save(self.model.state_dict(), model_path)
+
+    def load(self, model_path):
+        self.model.load_state_dict(torch.load(model_path))
+
+    @property
+    def weights(self):
+        return copy.deepcopy(self.model.state_dict())
+
+    @weights.setter
+    def weights(self, state):
+        self.model.load_state_dict(state)
+
+
+def _bn_act(num_features, activation, use_batch_norm=False):
+    # batch normal + activation
+    if use_batch_norm:
+        if activation is None:
+            return BatchNorm1d(num_features)
+        else:
+            return Sequential(BatchNorm1d(num_features), activation)
+    else:
+        return activation
+
+class NodeEmbedding(Module):
+    """
+    Node Embedding layer
+    """
+    def __init__(self, in_features, out_features, activation=Sigmoid(),
+                 use_batch_norm=False, bias=False):
+        super(NodeEmbedding, self).__init__()
+        self.linear = Linear(in_features, out_features, bias=bias)
+        self.activation = _bn_act(out_features, activation, use_batch_norm)
+
+    def forward(self, input):
+        output=self.linear(input)
+        output = self.activation(output)
+        return output
+
+
+class OLP(Module):
+    def __init__(self, in_features, out_features, activation=ELU(),
+                use_batch_norm=False, bias=False):
+        # One layer Perceptron
+        super(OLP, self).__init__()
+        self.linear = Linear(in_features, out_features, bias=bias)
+        self.activation = _bn_act(out_features, activation, use_batch_norm)
+
+    def forward(self,  input):
+        z = self.linear(input)
+        if self.activation:
+            z = self.activation(z)
+        return z
+
+class Gated_pooling(Module):
+
+    def __init__(self, in_features, out_features, activation=ELU(),
+                 use_batch_norm=False, bias=False):
+        super(Gated_pooling, self).__init__()
+        self.linear1 = Linear(in_features, out_features, bias=bias)
+        self.activation1 = _bn_act(out_features, activation, use_batch_norm)
+        self.linear2 = Linear(in_features, out_features, bias=bias)
+        self.activation2 = _bn_act(out_features, activation, use_batch_norm)
+
+    def forward(self,  input,graph_indices,node_counts):
+
+        z = self.activation1(self.linear1(input))*self.linear2(input)
+        graphcount=len(node_counts)
+        device=z.device
+        blank=torch.zeros(graphcount,z.shape[1]).to(device)
+        blank.index_add_(0, graph_indices, z)/node_counts.unsqueeze(1)
+        #output = self.activation2(self.linear2(blank)) ################对每个图加起来
+        return blank
+ 
+
+class GatedGraphConvolution(Module):
+    def __init__(self,n_node_feat, in_features, out_features, N_shbf ,N_srbf,n_grid_K,n_Gaussian, gate_activation=Sigmoid(),
+                 use_node_batch_norm=False, use_edge_batch_norm=False,
+                 bias=False, conv_type=0, MLP_activation=ELU()):
+        super(GatedGraphConvolution, self).__init__()
+        k1= n_Gaussian # k is the number of basis
+        k2=n_grid_K**3
+        self.linear1_vector = Linear(k1, out_features, bias=bias) # linear for combine sets
+        self.linear1_vector_gate = Linear(k1, out_features, bias=bias) # linear for combine sets
+        self.activation1_vector_gate = _bn_act(out_features, gate_activation, use_edge_batch_norm)
+        self.linear2_vector = Linear(k2, out_features, bias=bias) # linear for plane waves
+        self.linear2_vector_gate = Linear(k2, k2, bias=bias) # linear for plane waves
+        self.activation2_vector_gate = _bn_act(k2, gate_activation, use_edge_batch_norm)
+
+        self.linear_gate = Linear(in_features, out_features, bias=bias)
+        self.activation_gate = _bn_act(out_features, gate_activation, use_edge_batch_norm)
+
+        self.linear_MLP = Linear(in_features, out_features, bias=bias)
+        self.activation_MLP = _bn_act(out_features, MLP_activation, use_edge_batch_norm)
+
+   
+    def forward(self, input,nodes, edge_sources, edge_targets, rij ,combine_sets,plane_wave,cutoff):
+
+        ni = input[edge_sources].contiguous()
+        nj = input[edge_targets].contiguous()
+        rij=rij.unsqueeze(1).contiguous()
+        mask=rij<cutoff
+        delta= (ni-nj)/rij
+        final_fe=torch.cat([ni,nj,delta],dim=1)
+        del ni,nj,delta
+        torch.cuda.empty_cache()
+ 
+        e_gate = self.activation_gate(self.linear_gate(final_fe))
+        e_MLP = self.activation_MLP(self.linear_MLP(final_fe))
+
+        z1 = self.linear1_vector(combine_sets)
+        gate=self.activation2_vector_gate(self.linear2_vector_gate(plane_wave))
+        z2 = self.linear2_vector(plane_wave*gate)
+        z =  e_gate * e_MLP * (z1+z2) * mask
+        #z =  e_gate * e_MLP * mask
+        del z1,z2,e_gate,e_MLP
+        torch.cuda.empty_cache()
+        output = input.clone()
+        output.index_add_(0, edge_sources, z)
+        
+        return output
+
+class geo_CGNN(nn.Module):
+    def __init__(self,n_node_feat, n_hidden_feat,n_GCN_feat,conv_bias,N_block,node_activation, MLP_activation,use_node_batch_norm,use_edge_batch_norm, N_shbf ,N_srbf, cutoff,max_nei,n_MLP_LR,n_grid_K,n_Gaussian):
+
+        super(geo_CGNN, self).__init__()
+        self.cutoff=cutoff
+        self.N_block=N_block
+        node_activation=get_activation(node_activation)
+        MLP_activation=get_activation(MLP_activation)
+        self.embedding = NodeEmbedding(n_node_feat, n_hidden_feat)
+        n2v_concatent_feat = n_hidden_feat*3 #ni+nj+delta
+
+        self.conv = [GatedGraphConvolution(n_node_feat,n2v_concatent_feat, n_hidden_feat, N_shbf ,N_srbf,n_grid_K,n_Gaussian,
+                gate_activation=node_activation, # sigmoid
+                MLP_activation=MLP_activation, # Elu >1
+                use_node_batch_norm=use_node_batch_norm,
+                use_edge_batch_norm=use_edge_batch_norm,
+                bias=conv_bias)]
+
+        self.MLP_psi2n=[OLP(n_hidden_feat, n_hidden_feat, activation=MLP_activation, use_batch_norm=use_node_batch_norm, bias=conv_bias)]
+
+        self.conv += [GatedGraphConvolution(n_node_feat,n2v_concatent_feat, n_hidden_feat, N_shbf ,N_srbf,n_grid_K,n_Gaussian,
+                gate_activation=node_activation,
+                MLP_activation=MLP_activation,
+                use_node_batch_norm=use_node_batch_norm,
+                use_edge_batch_norm=use_edge_batch_norm,
+                bias=conv_bias) for _ in range(N_block-1)]
+        self.conv=ModuleList(self.conv)
+
+        self.MLP_psi2n = [OLP(n_hidden_feat, n_hidden_feat, activation=MLP_activation, use_batch_norm=use_node_batch_norm, bias=conv_bias) for _ in range(N_block)]
+        self.MLP_psi2n=ModuleList(self.MLP_psi2n)
+        
+        # gated pooling for every block
+        self.gated_pooling=[Gated_pooling(n_hidden_feat, n_GCN_feat, activation=MLP_activation ,use_batch_norm=use_node_batch_norm, bias=conv_bias) for _ in range(N_block)]
+        self.gated_pooling=ModuleList(self.gated_pooling)
+        
+        # final linear regression
+        self.linear_regression=[OLP(int(n_GCN_feat/2**(i-1)), int(n_GCN_feat/2**i) , activation=MLP_activation, use_batch_norm=use_node_batch_norm, bias=conv_bias) for i in range(1,n_MLP_LR)]
+        self.linear_regression += [OLP(int(n_GCN_feat/2**(n_MLP_LR-1)), 36 , activation=None, use_batch_norm=None, bias=conv_bias)]
+        self.linear_regression=ModuleList(self.linear_regression)
+        '''
+        # final linear regression
+        self.linear_regression=[OLP(int(n_GCN_feat/i), int(n_GCN_feat/(i+1)) , activation=MLP_activation, use_batch_norm=use_node_batch_norm, bias=conv_bias) for i in range(1,n_MLP_LR)]
+        self.linear_regression += [OLP(int(n_GCN_feat/n_MLP_LR), 1 , activation=None, use_batch_norm=None, bias=conv_bias)]
+        self.linear_regression=ModuleList(self.linear_regression) 
+        '''
+    
+    def forward(self,nodes,edge_sources,edge_targets,edge_distance,graph_indices,node_counts,combine_sets,plane_wave,output_graph=False):
+        x = self.embedding(nodes) 
+        Poolingresults=[]
+        
+        for i in range(self.N_block):
+            x = self.conv[i](x,nodes,  edge_sources, edge_targets,edge_distance,combine_sets,plane_wave,self.cutoff)
+
+            poo=self.gated_pooling[i](x,graph_indices,node_counts)
+            Poolingresults.append(poo)
+            x = self.MLP_psi2n[i](x)
+        graph_vec=torch.sum(torch.stack(Poolingresults),dim=0)
+        y=graph_vec
+        for lr in self.linear_regression:
+            y=lr(y)
+        if output_graph:
+            return y.squeeze(),graph_vec
+        else:
+            return y.squeeze()
